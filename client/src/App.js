5389522943e03a01df921c07782420a355f94ec4
yourdiebronze7 <sidnyjernov@hotmail.com>
2025-09-06 11:56:19 -0700

    feat: initial commit for client/src/App.js

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Chăm Sóc Sức Khỏe</h1>
+        <p>Đăng nhập để quản lý hồ sơ sức khỏe của bạn.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
